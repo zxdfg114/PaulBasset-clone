c7d8d6e2a33fb866524ea2fe77d27db829782603
zxdfg114@gmail.com <zxdfg114@gmail.com>
2022-11-03 17:46:05 +0900

    design : 디자인 디테일 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,47 +1,50 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 
-
-export default function Hero () {
- 
+export default function Hero() {
   return (
     <div className="hero">
       <Swiper
-      navigation={true}
-      spaceBetween={30}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      rewind={true}
-      loop={true}
-      pagination={{
-        type: "fraction",
-      }}
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}>
-      <SwiperSlide>
-      <div className="main1 main">
-        <p><i className="fa fa-bars"></i>추천메뉴</p>
-        <article>
-          <h1>HAPPY <br></br> HALLOWEEN</h1>
-          <p>폴 바셋 할로윈 시즌 음료</p>
-          <Link to={process.env.PUBLIC_URL +'/event/halloween'}>Detail View</Link>
-        </article>
-      </div>
-      </SwiperSlide>
-      <SwiperSlide>
-      <div className="main main2">
-        <p><i className="fa fa-bars"></i>추천메뉴</p>
-        <article>
-          <h1>Tea & Pistachio</h1>
-          <p>가을에 어울리는 티 & 피스타치오</p>
-          <Link to={process.env.PUBLIC_URL +'/event/teaandPistachio'}>Detail View</Link>
-        </article>
-      </div>
-      </SwiperSlide>
+        navigation={true}
+        spaceBetween={30}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        rewind={true}
+        loop={true}
+        pagination={{
+          type: "fraction",
+        }}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      >
+        <SwiperSlide>
+          <div className="main1 main">
+            <article>
+              <h1>
+                HAPPY <br></br> HALLOWEEN
+              </h1>
+              <p>폴 바셋 할로윈 시즌 음료</p>
+              <Link to={process.env.PUBLIC_URL + "/event/halloween"}>
+                Detail View
+              </Link>
+            </article>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="main main2">
+            <article>
+              <h1>Tea & Pistachio</h1>
+              <p>가을에 어울리는 티 & 피스타치오</p>
+              <Link to={process.env.PUBLIC_URL + "/event/teaandPistachio"}>
+                Detail View
+              </Link>
+            </article>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
-  )
+  );
 }
--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay, Mousewheel } from 'swiper';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  Mousewheel,
+} from "swiper";
 import { Link } from "react-router-dom";
 
 export default function MainBanner() {
-  console.log(process.env.PUBLIC_URL)
+  console.log(process.env.PUBLIC_URL);
   const events = [
     {
       name: "신메뉴 크라운 추가적립 이벤트",
       description: "크라운 오더로 크라운 주문시 크라운 추가 적립",
       ImgSrc: "/image/mainBanner00.jpg",
-      link : "crown"
+      link: "crown",
     },
     {
       name: "자동충전 이벤트",
       description: "5만원 이상 자동 충전시, 1+1 쿠폰 증정",
       ImgSrc: "/image/mainBanner01.jpg",
-      link : "autocharge"
+      link: "autocharge",
     },
     {
       name: "BeanInfo",
@@ -36,14 +43,14 @@ export default function MainBanner() {
         {events.map((data, i) => {
           return (
             <SwiperSlide key={i}>
-              <Link to ={process.env.PUBLIC_URL +`/event/${events[i].link}`}>
-              <div className="main-banner-item" key={i}>
-                <img src={process.env.PUBLIC_URL+events[i].ImgSrc} alt="" />
-                <article>
-                  <h1>{events[i].name}</h1>
-                  <p>{events[i].description}</p>
-                </article>
-              </div>
+              <Link to={process.env.PUBLIC_URL + `/event/${events[i].link}`}>
+                <div className="main-banner-item" key={i}>
+                  <img src={process.env.PUBLIC_URL + events[i].ImgSrc} alt="" />
+                  <article>
+                    <h1>{events[i].name}</h1>
+                    <p>{events[i].description}</p>
+                  </article>
+                </div>
               </Link>
             </SwiperSlide>
           );
